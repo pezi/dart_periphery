3998974d1f74ed8fda55055f5861753a3a618b79
Peter Sauer <peter.sauer@sciencesoft.at>
2025-02-22 10:12:11 +0100

    Extend vl53l0x exmaple

--- example/i2c_vl53l0x.dart
+++ example/i2c_vl53l0x.dart
@@ -11,13 +11,54 @@ import 'dart:io';
 /// https://wiki.seeedstudio.com/Grove-Time_of_Flight_Distance_Sensor-VL53L0X/
 ///
 
-void main() {
+void main(List<String> args) {
   // Select the right I2C bus number /dev/i2c-?
   // 1 for Raspberry Pi, 0 for NanoPi (Armbian), 2 Banana Pi (Armbian), 4 BPI-F3
   var i2c = I2C(1);
   var v = VL53L0X(i2c);
-  while (true) {
-    print("Distance [mm]: ${v.getRange()}");
-    sleep(Duration(seconds: 1));
+  if (args.length != 1) {
+    print("missing parameter: simple or continous");
+  }
+  if (args.first == 'simple') {
+    print("Simple mode");
+    while (true) {
+      print("Distance [mm]: ${v.getRange()}");
+      sleep(Duration(seconds: 1));
+    }
+  } else {
+    print("Continous mode");
+    // Optionally adjust the measurement timing budget to change speed and accuracy.
+    // See the example here for more details:
+    //   https://github.com/pololu/vl53l0x-arduino/blob/master/examples/Single/Single.ino
+    // For example a higher speed but less accurate timing budget of 20ms:
+    // vl53.measurement_timing_budget = 20000
+    // Or a slower but more accurate timing budget of 200ms:
+    v.setMeasurementTimingBudget(200000);
+    v.startContinuous();
+    try {
+      // try to adjust the sleep time (simulating program doing something else)
+      //  # and see how fast the sensor returns the range
+      sleep(Duration(microseconds: 100));
+      var time = DateTime.now().millisecondsSinceEpoch;
+      var range = v.getRange();
+      var div = DateTime.now().millisecondsSinceEpoch - time;
+      print("Range: $range mm $div ms");
+    } on Exception catch (e) {
+      print('Exception details:\n $e');
+    } finally {
+      v.startContinuous();
+    }
   }
 }
+
+/*
+# Main loop will read the range and print it every second.
+with vl53.continuous_mode():
+    while True:
+        # try to adjust the sleep time (simulating program doing something else)
+        # and see how fast the sensor returns the range
+        time.sleep(0.1)
+
+        curTime = time.time()
+        print("Range: {0}mm ({1:.2f}ms)".format(vl53.range, time.time() - curTime))
+        */
